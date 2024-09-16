20a8edec4214d014179c8a274c51ade847ca43d9
julianfssen <julianfssen@gmail.com>
2024-09-16 15:25:54 +0800

    Add sample to fetch events

--- index.ts
+++ index.ts
@@ -1,13 +1,24 @@
-import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
+import {
+  AnchorProvider,
+  DISCRIMINATOR_SIZE,
+  Program,
+  Wallet,
+  utils,
+} from "@coral-xyz/anchor";
 import { IDL, type Perpetuals } from "./idl";
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+
+const JUPITER_PERPETUALS_PROGRAM = new PublicKey(
+  "PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu",
+);
 
 async function getCustodyAssets() {
   try {
     const connection = new Connection("https://api.mainnet-beta.solana.com");
+
     const program = new Program<Perpetuals>(
       IDL,
-      new PublicKey("PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu"),
+      JUPITER_PERPETUALS_PROGRAM,
       new AnchorProvider(
         connection,
         new Wallet(Keypair.generate()),
@@ -33,4 +44,59 @@ async function getCustodyAssets() {
   }
 }
 
+const JUPITER_PERPETUALS_EVENT_AUTHORITY = new PublicKey(
+  "37hJBDnntwqhGbK7L6M1bLyvccj4u55CCUiLPdYkiqBN",
+);
+
+async function getEvents() {
+  const connection = new Connection("https://api.mainnet-beta.solana.com");
+
+  const program = new Program<Perpetuals>(
+    IDL,
+    JUPITER_PERPETUALS_PROGRAM,
+    new AnchorProvider(
+      connection,
+      new Wallet(Keypair.generate()),
+      AnchorProvider.defaultOptions(),
+    ),
+  );
+
+  const confirmedSignatureInfos = await connection.getSignaturesForAddress(
+    JUPITER_PERPETUALS_EVENT_AUTHORITY,
+  );
+
+  const successSignatures = confirmedSignatureInfos
+    .filter(({ err }) => err === null)
+    .map(({ signature }) => signature);
+
+  const txs = await connection.getTransactions(successSignatures, {
+    commitment: "confirmed",
+    maxSupportedTransactionVersion: 0,
+  });
+
+  const allEvents = txs.flatMap((tx) => {
+    return tx?.meta?.innerInstructions?.flatMap((ix) => {
+      return ix.instructions.map((iix, ixIndex) => {
+        const ixData = utils.bytes.bs58.decode(iix.data);
+        const eventData = utils.bytes.base64.encode(
+          ixData.subarray(DISCRIMINATOR_SIZE),
+        );
+        const event = program.coder.events.decode(eventData);
+
+        return {
+          event,
+          ixIndex,
+          tx,
+        };
+      });
+    });
+  });
+
+  // Sample of getting an event emitted from the Jupiter perps platform
+  const closePositionRequestEvents = allEvents.filter(
+    (data) => data?.event?.name === "ClosePositionRequestEvent",
+  );
+}
+
 getCustodyAssets();
+getEvents();
