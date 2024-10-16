24373455dd697f497da844b650157e0430a40be9
julianfssen <julianfssen@gmail.com>
2024-10-16 10:10:16 +0800

    Add method to calculate pool APY

--- index.ts
+++ index.ts
@@ -13,20 +13,24 @@ const JUPITER_PERPETUALS_PROGRAM = new PublicKey(
   "PERPHjGBqRHArX4DySjwM6UJHiR3sWAatqfdBS2qQJu",
 );
 
+const JLP_POOL_ACCOUNT = new PublicKey(
+  "5BUwFW4nRbftYTDMbgxykoFWqWHPzahFSNAaaaJtVKsq",
+);
+
+const connection = new Connection("https://api.mainnet-beta.solana.com");
+
+const program = new Program<Perpetuals>(
+  IDL,
+  JUPITER_PERPETUALS_PROGRAM,
+  new AnchorProvider(
+    connection,
+    new Wallet(Keypair.generate()),
+    AnchorProvider.defaultOptions(),
+  ),
+);
+
 async function getCustodyAssets() {
   try {
-    const connection = new Connection("https://api.mainnet-beta.solana.com");
-
-    const program = new Program<Perpetuals>(
-      IDL,
-      JUPITER_PERPETUALS_PROGRAM,
-      new AnchorProvider(
-        connection,
-        new Wallet(Keypair.generate()),
-        AnchorProvider.defaultOptions(),
-      ),
-    );
-
     const solCustody = await program.account.custody.fetch(
       new PublicKey("7xS2gz2bTp3fwCC7knJvUWTEU9Tycczu6VhJYKgi1wdz"),
     );
@@ -150,5 +154,15 @@ async function getEvents() {
   );
 }
 
-getCustodyAssets();
-getEvents();
+async function getPoolApy() {
+  const pool = await program.account.pool.fetch(JLP_POOL_ACCOUNT);
+
+  const poolApr = pool.poolApr.feeAprBps.toNumber() / 100;
+
+  const compoundToAPY = (apr: number, frequency = 365) => {
+    const apy = (Math.pow(apr / 100 / frequency + 1, frequency) - 1) * 100;
+    return apy;
+  };
+
+  console.log("Pool APY (%):", compoundToAPY(poolApr));
+}
